d1327c621c855f2c8b1fb313263cf21cbf656500
WILLIAM JONATHAN HANDOKO <108049958+Monkeycode21@users.noreply.github.com>
2024-07-14 11:49:38 +0700

    Initial Commit

---
+++ MPTest/MPTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MPTestApp.swift
+//  MPTest
+//
+//  Created by William Handoko on 14/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MPTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
